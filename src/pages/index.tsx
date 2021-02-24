05f6865209e70d8a9d7a31f0f2b5be2c830124e9
Marlon Strong <tsenane14@gmail.com>
2021-02-24 16:32:13 +0200

    :construction: feat-> added homepage title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,15 @@ import styles from '../styles/pages/Home.module.css'
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 
+
 export default function Home() {
   return (
     <div className={styles.container}>
-      
+     <Head>
+       <title>
+         Inicio | move.it
+       </title>
+       </Head> 
     <ExperienceBar/>
 
     <section>
