958f54867f640dbe0f08dbf0b6f6de8fe40a8fab
Marlon Strong <tsenane14@gmail.com>
2021-02-24 14:47:04 +0200

    :construction: feat-> created /styles/pages/Home.modul.css

---
+++ src/components/Profile.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+
+export function Profile(){
+    return(
+        <div>
+            <img src="https://github.com/paichato.png" alt="Marlon D Jesus"/>
+            <div>
+                <strong>Marlon D Jesus</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    )
+}
