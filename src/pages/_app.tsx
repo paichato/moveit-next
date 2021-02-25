a44a2c187af2fd1a640cea342039ed52a282efa1
Marlon Strong <tsenane14@gmail.com>
2021-02-25 23:56:12 +0200

    :construction: feat-> added contextProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
+import { ChallengesProvider } from '../contexts/ChallengeContext';
 import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChallengesProvider>
+  <Component {...pageProps} />
+  </ChallengesProvider>
+  )
 }
 
 export default MyApp
