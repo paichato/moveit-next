507f4280ba36c667167a0e058e966e20001ab0ba
Marlon Strong <tsenane14@gmail.com>
2021-02-25 22:23:38 +0200

    :construction: feat-> added state hasfinished

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -7,6 +7,7 @@ export function Countdown(){
 
     const [time, settime] = useState(25*60);
     const [isActive, setIsActive] = useState(false);
+    const [hasfinished, sethasfinished] = useState(false)
 
     const minutes= Math.floor(time/60);
     const seconds= time % 60;
@@ -29,6 +30,9 @@ export function Countdown(){
             countdownTimeout=setTimeout(() => {
                 settime(time-1);
             }, 1000);
+        } else if(isActive && time===0){
+            sethasfinished(true);
+            setIsActive(false);
         }
     }, [isActive, time])
 
