5edc5e7f6ae6b08b9c9107f62b5758d35af3be92
Marlon Strong <tsenane14@gmail.com>
2021-02-25 20:13:40 +0200

    :ok_hand: feat-> changing active state to isActive

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/component/Countdown.module.css';
 export function Countdown(){
 
     const [time, settime] = useState(25*60);
-    const [active, setactive] = useState(false);
+    const [isActive, setIsActive] = useState(false);
 
     const minutes= Math.floor(time/60);
     const seconds= time % 60;
@@ -13,16 +13,16 @@ export function Countdown(){
     const [secondsLeft,secondsRight]= String(seconds).padStart(2,'0').split('');
 
     function startCountdown(){
-        setactive(true);
+        setIsActive(true);
     }
 
     useEffect(() => {
-        if(active && time>0){
+        if(isActive && time>0){
             setTimeout(() => {
                 settime(time-1);
             }, 1000);
         }
-    }, [active, time])
+    }, [isActive, time])
 
     return(
         <div>
