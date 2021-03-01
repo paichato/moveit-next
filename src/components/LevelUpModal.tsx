57bed2fbb045891e9f1f9382b3575e5ea92f6310
Marlon Strong <tsenane14@gmail.com>
2021-03-01 14:57:02 +0200

    :construction: feat -> added user level to Modal

--- src/components/LevelUpModal.tsx
+++ src/components/LevelUpModal.tsx
@@ -1,10 +1,16 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengeContext';
 import styles from '../styles/component/LevelUpModal.module.css';
 
 export function LevelUpModal(){
+
+    const {level}= useContext(ChallengesContext);
+
+
     return(
         <div className={styles.overlay}>
             <div className={styles.container}>
-                <header>2</header>
+                <header>{level}</header>
                 <strong>Parabens!</strong>
                 <p>Voce alcançoun um novo level</p>
                 <button type="button">
