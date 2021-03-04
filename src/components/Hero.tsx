fee273269b4e44c4aeb6e89c658f15b94da2d84a
Marlon Strong <tsenane14@gmail.com>
2021-03-04 18:44:08 +0200

    :bug: fixed routes working

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,130 +1,131 @@
-import styles from '../styles/component/Hero.module.css';
-import {BsBoxArrowInRight} from 'react-icons/bs';
-import {AiFillGithub} from 'react-icons/ai';
-import { useContext, useEffect, useState } from 'react';
-import { api } from '../api';
-import { toast} from 'react-toastify';
-import { LogginContext } from '../contexts/LogginContext';
+import styles from "../styles/component/Hero.module.css";
+import { BsBoxArrowInRight } from "react-icons/bs";
+import { AiFillGithub } from "react-icons/ai";
+import { useContext, useEffect, useState } from "react";
+import { api } from "../api";
+import { toast } from "react-toastify";
+import { LogginContext } from "../contexts/LogginContext";
+import Router from "next/router";
 
-
-interface userProps{
-    login:string;
-    avatar_url:string;
-    name:string;
-
+interface userProps {
+  login: string;
+  avatar_url: string;
+  name: string;
 }
 
+export const guest: userProps = {
+  login: "guest",
+  avatar_url:
+    "https://upload.wikimedia.org/wikipedia/commons/e/e1/Strawberries.jpg",
+  name: "guest user",
+};
 
-export const guest:userProps={login:"guest", avatar_url:"https://upload.wikimedia.org/wikipedia/commons/e/e1/Strawberries.jpg",name:"guest user"}
+export let user: userProps = { login: null, avatar_url: null, name: null };
 
-export let user:userProps ={login:"",avatar_url:"", name:""};
+export function Hero() {
+  const { isLogged, fazerLogin } = useContext(LogginContext);
 
+  const [gituser, setgituser] = useState("");
+  const [input, setinput] = useState("");
 
-
-export function Hero(){
-
-    const { isLogged, fazerLogin } = useContext(LogginContext);
-
-    const [gituser, setgituser] = useState("");
-    const [input, setinput] = useState("");
-
-
+  useEffect(() => {
+    fazerLogin();
     
-    useEffect(() => {
-        fazerLogin();
-    }, [user])
-    
+  }, [user]);
 
-    async function handleSubmitUser(e:React.FormEvent<HTMLFormElement>){
-        e.preventDefault();
+  async function handleSubmitUser(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
 
-        
-        if(input===""){
-            console.log("insere username ou gitname");
-            toast.dark('❌ Insere username ou gitname!', {
-                position: "bottom-left",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                });
-        }else{
-            setgituser(input);
-           await api.get(`/${input}?`)
-                .then(function(response){
-                    user=response.data;
+    if (input === "") {
+      console.log("insere username ou gitname");
+      toast.dark("❌ Insere username ou gitname!", {
+        position: "bottom-left",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    } else {
+      setgituser(input);
+      await api
+        .get(`/${input}?`)
+        .then(function (response) {
+          user = response.data;
 
-                    toast.success('✅Usuario encontrado -> logando...', {
-                        position: "bottom-left",
-                        autoClose: 5000,
-                        hideProgressBar: false,
-                        closeOnClick: true,
-                        pauseOnHover: true,
-                        draggable: true,
-                        progress: undefined,
-                        });
+          toast.success("✅Usuario encontrado -> logando...", {
+            position: "bottom-left",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
 
-                    console.log(response.data);
-                    console.log(user.name);
+          console.log(response.data);
+          console.log(user.name);
 
-                    // logar();
+          // logar();
 
-                    console.log(isLogged);
+          console.log(isLogged);
+        })
+        .catch(function (error) {
+          user = guest;
+          user.login = input;
+          console.log(error);
 
-                }).catch(function(error){
-                    user=guest;
-                    user.login=input;
-                    console.log(error);
+          toast.error(`⚠ Entrando como ${user.login} {guest}`, {
+            position: "bottom-left",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
 
-                    toast.error(`⚠ Entrando como ${user.login} {guest}`, {
-                        position: "bottom-left",
-                        autoClose: 5000,
-                        hideProgressBar: false,
-                        closeOnClick: true,
-                        pauseOnHover: true,
-                        draggable: true,
-                        progress: undefined,
-                        });
+          console.log(user.name);
 
-                    console.log(user.name);
-
-                    // logar();
-                    console.log(isLogged);
-
-                }).then(function(){
-                    setinput("");
-                })
-        }
-    
-       
-
+          // logar();
+          console.log(isLogged);
+        })
+        .then(function () {
+          setinput("");
+        });
     }
+  }
 
-    return(
-        <div className={styles.container} >
-            
-            <img src="movit-hero-bg.png" alt="background moveit"/>
-            <div>
-                <img src="Moveit-bg.png" alt="" />
-                <h3>Bem vindo</h3>
-                <div className={styles.git}>
-                    <AiFillGithub className={styles.icon}/>
-                <p>Coloque o seu  nome do github  ou 
-um nome aleatorio</p>
-                </div>
-                
-                <section>
-                    <form onSubmit={handleSubmitUser}>
-                        <input onChange={(e:React.FormEvent<HTMLInputElement>)=>{setinput(e.currentTarget.value)}} value={input} type="text" name="username" id="" className={styles.input}/>
-                        <button type="submit" ><BsBoxArrowInRight className={styles.icon}/></button>
-                    </form>
-                </section>
-                
-
-            </div>
-            
+  return (
+    <div className={styles.container}>
+      <img src="movit-hero-bg.png" alt="background moveit" />
+      <div>
+        <img src="Moveit-bg.png" alt="" />
+        <h3>Bem vindo</h3>
+        <div className={styles.git}>
+          <AiFillGithub className={styles.icon} />
+          <p>Coloque o seu nome do github ou um nome aleatorio</p>
         </div>
-    )
+
+        <section>
+          <form onSubmit={handleSubmitUser}>
+            <input
+              onChange={(e: React.FormEvent<HTMLInputElement>) => {
+                setinput(e.currentTarget.value);
+              }}
+              value={input}
+              type="text"
+              name="username"
+              id=""
+              className={styles.input}
+            />
+            <button type="submit">
+              <BsBoxArrowInRight className={styles.icon} />
+            </button>
+          </form>
+        </section>
+      </div>
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,93 +1,102 @@
 import { ExperienceBar } from "../components/ExperienceBar";
-import Head from 'next/head';
+import Head from "next/head";
 import { Profile } from "../components/Profile";
-import styles from '../styles/pages/Home.module.css'
+import styles from "../styles/pages/Home.module.css";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ChallengeBox } from "../components/ChallengeBox";
 import { CountdownProvider } from "../contexts/CountdownContext";
-import {GetServerSideProps} from 'next'
+import { GetServerSideProps } from "next";
 import { ChallengesProvider } from "../contexts/ChallengeContext";
-import { Hero,  user } from "../components/Hero";
+import { Hero, user } from "../components/Hero";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import { LogginContext, LogginProvider } from "../contexts/LogginContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
-interface HomeProps{
-  level:number;
-  currentXp:number;
-  challengesCompleted:number;
-
+interface HomeProps {
+  level: number;
+  currentXp: number;
+  challengesCompleted: number;
 }
 
-
-export default function Home(props:HomeProps) {
-
+export default function Home(props: HomeProps) {
   const { isLogged } = useContext(LogginContext);
-  
+
+  const [logado, setlogado] = useState(isLogged);
+
   // useEffect(() => {
-    
+
   // }, [isLogged])
+  const router = useRouter();
+
+  useEffect(() => {
+   console.log("logado");
+  }, [isLogged]);
 
   return (
     <>
-    <LogginProvider  >
-    <ChallengesProvider level={props.level} currentXp={props.currentXp} challengesCompleted={props.challengesCompleted}>
-     
-     <ToastContainer
-                position="top-center"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                />
-
-        {isLogged?(<div className={styles.container}>
-     <Head>
-       <title>
-         Inicio | move.it
-       </title>
-       </Head> 
       
-    <ExperienceBar/>
+        <ChallengesProvider
+          level={props.level}
+          currentXp={props.currentXp}
+          challengesCompleted={props.challengesCompleted}
+        >
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+       
+            
+        
 
-    <CountdownProvider>
-      <section>
-        <div>
-          <Profile/>
-          <CompletedChallenges/>
-          <Countdown/>
-          
-        </div>
-        <div>
-          <ChallengeBox/>
-        </div>
-      </section>
-    </CountdownProvider>
-  </div> ):(<Hero/>)}
+      
+        
+         <div className={styles.container}>
+            <Head>
+              <title>Inicio | move.it</title>
+            </Head>
+
+            <ExperienceBar />
+
+            <CountdownProvider>
+              <section>
+                <div>
+                  <Profile />
+                  <CompletedChallenges />
+                  <Countdown />
+                </div>
+                <div>
+                  <ChallengeBox />
+                </div>
+              </section>
+            </CountdownProvider>
+          </div>
+         
+         )
+        </ChallengesProvider>
      
-  </ChallengesProvider>
-  </LogginProvider>
     </>
-  )
+  );
 }
 
-export const getServerSideProps:GetServerSideProps = async (ctx) => {
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentXp, challengesCompleted } = ctx.req.cookies;
 
- 
-
-  const {level,currentXp, challengesCompleted}=ctx.req.cookies;
-
-  return{
-    props:{
-      level:Number(level),
-      currentXp:Number(currentXp),
-       challengesCompleted:Number(challengesCompleted)
-    }
-  }
-}
+  return {
+    props: {
+      level: Number(level),
+      currentXp: Number(currentXp),
+      challengesCompleted: Number(challengesCompleted),
+    },
+  };
+};
