a050db9134bebeba1ab0a2fa378c8f89cad67aac
Marlon Strong <tsenane14@gmail.com>
2021-03-06 16:02:07 +0200

    :construction: feat -> added styles to config button

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,9 +6,11 @@ import { FiShare2 } from "react-icons/fi";
 import { useRouter } from "next/router";
 import { useContext } from "react";
 import { LogoutContext } from "../contexts/LogoutContext";
-import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/menu";
+import { Menu, MenuButton, MenuDivider, MenuItem, MenuList } from "@chakra-ui/menu";
 import { Button } from "@chakra-ui/button";
 import {FaHourglassStart} from 'react-icons/fa'
+import {BsClockHistory} from 'react-icons/bs';
+import {GiClockwork} from 'react-icons/gi';
 
 export function Sidebar() {
   const router = useRouter();
@@ -46,9 +48,12 @@ export function Sidebar() {
             <IoMdOptions />
           </MenuButton>
           <MenuList style={{background:"var(--white)",boxShadow:"0 0 20px rgba(0,0,200,0.2)", outline:"none", border:"none"}}>
-            <MenuItem  icon={<FaHourglassStart/> } style={{ color:"darkgoldenrod"}}>6 Seg</MenuItem>
-            <MenuItem icon={<FaHourglassStart/>} style={{ color:"var(--blue)"}}>25 min</MenuItem>
-            <MenuItem icon={<FaHourglassStart/>}style={{ color:"var(--title)"}}>1 hora</MenuItem>
+            <MenuItem   icon={<FaHourglassStart style={{fill:"var(--blue-dark)"}}/> } style={{ color:"var(--title)", fontWeight:"bolder"}}>DEMO - 5 s</MenuItem>
+            <MenuDivider style={{color:"var(--gray-line)"}}/>
+            <MenuItem  icon={<BsClockHistory style={{fill:"var(--blue-dark)"}}/>} style={{ color:"var(--title)", fontWeight:"bolder"}}>POMODORO - 25 m</MenuItem>
+            <MenuDivider/>
+            <MenuItem icon={<GiClockwork style={{fill:"var(--blue-dark)"}}/>}style={{ color:"var(--title)", fontWeight:"bolder"}}>ADVANCED - 1 h</MenuItem>
+           
           </MenuList>
         </Menu>
         </button>
