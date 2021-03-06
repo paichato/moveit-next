8342f41b8e8f80c19864da41d9d321a18fb4c04d
Marlon Strong <tsenane14@gmail.com>
2021-03-06 20:00:53 +0200

    :construction: feat -> bugs on dark button

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,48 +1,50 @@
 import styles from "../styles/component/Sidebar.module.css";
-import Link from "next/link";
 import { BiArrowBack } from "react-icons/bi";
 import { IoMdOptions } from "react-icons/io";
-import { FiShare2 } from "react-icons/fi";
 import { useRouter } from "next/router";
-import { AriaAttributes, ReactEventHandler, useContext, useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { LogoutContext } from "../contexts/LogoutContext";
-import { Menu, MenuButton, MenuDivider, MenuItem, MenuList } from "@chakra-ui/menu";
+import {
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/menu";
 import { Button } from "@chakra-ui/button";
-import {FaHourglassStart} from 'react-icons/fa'
-import {BsClockHistory} from 'react-icons/bs';
-import {BiShare} from 'react-icons/bi';
-import {GiClockwork} from 'react-icons/gi';
+import { FaHourglassStart } from "react-icons/fa";
+import { BsClockHistory } from "react-icons/bs";
+import { BiShare } from "react-icons/bi";
+import { VscColorMode } from "react-icons/vsc";
+import { GiClockwork } from "react-icons/gi";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import { CountdownContext, CountdownProvider } from "../contexts/CountdownContext";
-
+import { CountdownContext } from "../contexts/CountdownContext";
 
 const theme = extendTheme({
-    styles: {
-      global: (props) => ({
-        "html, body": {
-            fontFamily: "Inter",
-            color: props.colorMode === "dark" ? "white" : "var(--text)",
-            background:props.colorMode === "dark"? "white": "var(--background)",
-            lineHeight: "tall",
-        },
-        a: {
-          color: props.colorMode === "dark" ? "teal.300" : "var(--white)",
-        },
-        svg:{
-            fill:"var(--white)",
-        }
-      }),
-    },
-  })
+  styles: {
+    global: (props) => ({
+      "html, body": {
+        fontFamily: "Inter",
+        color: props.colorMode === "dark" ? "white" : "var(--text)",
+        background: props.colorMode === "dark" ? "white" : "var(--background)",
+        lineHeight: "tall",
+      },
+      a: {
+        color: props.colorMode === "dark" ? "teal.300" : "var(--white)",
+      },
+      svg: {
+        fill: "var(--white)",
+      },
+    }),
+  },
+});
 
 export function Sidebar() {
   const router = useRouter();
   const { show, mostrar } = useContext(LogoutContext);
-  const {setCTime}=useContext(CountdownContext);
+  const { setCTime } = useContext(CountdownContext);
 
-  useEffect(() => {
-      
-  }, [setCTime])
+  useEffect(() => {}, [setCTime]);
 
   function handleLogout() {
     //prompt confirmation modal
@@ -52,11 +54,11 @@ export function Sidebar() {
     console.log("mostrar");
   }
 
-  function handleConfig(e:React.FormEvent<HTMLButtonElement>) {
+  function handleConfig(e: React.FormEvent<HTMLButtonElement>) {
     //prompt config modal to choose time
     //if choosen switch time
     console.log(e.currentTarget.value);
-   
+
     setCTime(Number(e.currentTarget.value));
 
     // (e: React.FormEvent<HTMLInputElement>) => {
@@ -69,36 +71,74 @@ export function Sidebar() {
     //
   }
 
- 
-
   return (
     <>
       <div className={styles.container}>
         <button onClick={handleLogout}>
           <BiArrowBack />
-        </button  >
-   
-   <button style={{background:"var(--blue)"}}>
-       <ChakraProvider theme={theme}>
-          
-        <Menu isLazy placement="left" >
-          <MenuButton as={Button} style={{ background: "var(--blue)", alignItems:"center", justifyContent:"center", display:"flex" }}>
-            <IoMdOptions />
-          </MenuButton>
-          <MenuList   style={{background:"var(--white)",boxShadow:"0 0 20px rgba(0,0,200,0.2)", outline:"none", border:"none"}}>
-            <MenuItem  role="menuitemradio" value={0}  onClick={handleConfig}   icon={<FaHourglassStart style={{fill:"var(--blue-dark)"}}/> } style={{ color:"var(--title)", fontWeight:"bolder"}}>DEMO - 5 s</MenuItem>
-            <MenuDivider style={{color:"var(--gray-line)"}}/>
-            <MenuItem value={1} onClick={handleConfig}  icon={<BsClockHistory style={{fill:"var(--blue-dark)"}}/>} style={{ color:"var(--title)", fontWeight:"bolder"}}>POMODORO - 25 m</MenuItem>
-            <MenuDivider/>
-            <MenuItem value={2} onClick={handleConfig} icon={<GiClockwork style={{fill:"var(--blue-dark)"}}/>}style={{ color:"var(--title)", fontWeight:"bolder"}}>ADVANCED - 1 h</MenuItem>
-           
-          </MenuList>
-        </Menu>
-     
-        </ChakraProvider>
         </button>
-        <button>
+
+        <button style={{ background: "var(--blue)" }}>
+          <ChakraProvider theme={theme}>
+            <Menu isLazy placement="left">
+              <MenuButton
+                as={Button}
+                style={{
+                  background: "var(--blue)",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  display: "flex",
+                }}
+              >
+                <IoMdOptions />
+              </MenuButton>
+              <MenuList
+                style={{
+                  background: "var(--white)",
+                  boxShadow: "0 0 20px rgba(0,0,200,0.2)",
+                  outline: "none",
+                  border: "none",
+                }}
+              >
+                <MenuItem
+                  role="menuitemradio"
+                  value={0}
+                  onClick={handleConfig}
+                  icon={
+                    <FaHourglassStart style={{ fill: "var(--blue-dark)" }} />
+                  }
+                  style={{ color: "var(--title)", fontWeight: "bolder" }}
+                >
+                  DEMO - 5 s
+                </MenuItem>
+                <MenuDivider style={{ color: "var(--gray-line)" }} />
+                <MenuItem
+                  value={1}
+                  onClick={handleConfig}
+                  icon={<BsClockHistory style={{ fill: "var(--blue-dark)" }} />}
+                  style={{ color: "var(--title)", fontWeight: "bolder" }}
+                >
+                  POMODORO - 25 m
+                </MenuItem>
+                <MenuDivider />
+                <MenuItem
+                  value={2}
+                  onClick={handleConfig}
+                  icon={<GiClockwork style={{ fill: "var(--blue-dark)" }} />}
+                  style={{ color: "var(--title)", fontWeight: "bolder" }}
+                >
+                  ADVANCED - 1 h
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </ChakraProvider>
+        </button>
+        <button style={{ background: "var(--blue)" }}>
           <BiShare />
+        </button>
+
+        <button style={{ fill: "var(--title)" }}>
+          <VscColorMode />
         </button>
       </div>
     </>
