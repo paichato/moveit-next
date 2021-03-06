1c4a980307b58feb7de560e9f0939d449c71459f
Marlon Strong <tsenane14@gmail.com>
2021-03-06 05:06:08 +0200

    :construction: feat -> added logica instrunctions to aply next

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,8 +3,18 @@ import Link from 'next/link'
 import {BiArrowBack} from 'react-icons/bi'
 import {IoMdOptions} from 'react-icons/io'
 import {FiShare2} from 'react-icons/fi'
+import { useRouter } from 'next/router'
 
 export function Sidebar(){
+
+    const router =useRouter();
+
+    function handleLogout(){
+        //prompt confirmation modal
+        //if yes -> remove cookies && router.push('/')
+        //if no -> close modal
+    }
+
     return(
         <>
         <div className={styles.container}>
