3f90d80a438537a28daa0bbd7da2fdb215d144be
Marlon Strong <tsenane14@gmail.com>
2021-03-06 11:54:12 +0200

    :construction: feat -> adding Logout files

---
+++ src/components/LogoutModal.tsx
@@ -0,0 +1,18 @@
+import {GiConfirmed, GiCancel} from 'react-icons/gi';
+import styles from '../styles/component/LogoutModal.module.css'
+
+
+export function LogoutModal(){
+    return(
+        <div className={styles.overlay}>
+            <div className={styles.container}>
+                <header>Sair</header>
+                <strong>Tem certeza que deseja sair</strong>
+                <section>
+                <button><GiConfirmed/></button>
+                <button><GiCancel/></button>
+                </section>
+            </div>
+        </div>
+    )
+}
